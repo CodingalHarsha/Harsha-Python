6e070522387302d1d49b8a645f3a791a27523855
CodingalHarsha <sriharsha.dse@gmail.com>
2026-09-18 19:42:51 +0200

    star

---
+++ Star.py
@@ -0,0 +1,28 @@
+import turtle
+
+turtle.Screen().bgcolor("White")
+board = turtle.Turtle()
+
+
+board.forward(150)
+
+board.left(120)
+board.forward(150)
+
+board.left(120)
+board.forward(150)
+
+board.penup()
+board.right(150)
+board.forward(100)
+
+board.pendown()
+board.right(90)
+board.forward(150)
+
+board.right(120)
+board.forward(150)
+
+board.right(120)
+board.forward(150)
+turtle.done()
